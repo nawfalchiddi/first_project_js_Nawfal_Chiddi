64edb2e4395dfc7da49e1c67c927c3e62424f985
nawfalchidd <nawfalchiddi@icloud.com>
2026-06-20 08:59:37 -0700

    dealing with the problems of change the password and the log in

--- public/js/script.js
+++ public/js/script.js
@@ -6,12 +6,12 @@
 const userDatabase = [];
 
 function main() {
-    let running = true;
+    let process = true;
 
-    while (running) {
-        const choice = prompt("welcome to the banking System\nSelect an option:\n1-sign in \n2-Login\n3-Change Password\n4-Exit");
+    while (process) {
+        const action = prompt("welcome to the banking System\nSelect an option:\n1-sign in \n2-Login\n3-Change Password\n4-Exit");
 
-        switch (choice){
+        switch (action){
             case "1":
           signIn();
             break;
@@ -22,57 +22,65 @@ function main() {
           changePassword();
              break;
             case "4":
-                  alert("have a good day");
-           running = false;
+                  alert("see you next time");
+           process = false;
          break;
             default:
-        alert("you need to select only one number");
+        alert("selet the one of the numbers you see");
         }
     }
 }
 
 
  function signIn(){
-    while (true) {
-       let name = prompt("Enter your name :")
-        if (name && /^[A-Za-z]+$/.test(name)) {
-            break;
+     
+    
+     while (true) {
+         name = prompt("Enter your name :")
+         if (name && /^[A-Za-z]+$/.test(name)) {
+             break;
+            }
+            else{
+                alert(" it must contain only letters  and cannot be empty.");
+            }
         }
-        else{
-             alert(" it must contain only letters  and cannot be empty.");
-        }
-    }
- 
-
+        
+        
+        
     while(true){
-        let email = prompt("enter an email")
-        if (email.length > 7 && email.includes("@")) {
-            const doesEmailExist = userDatabase.some(Element => Element.email === email)
-         if(doesEmailExist){
-            alert("sorry but this email already exist")
-           
-        } break
-    }
-       
-        else{
-            alert("the email must contain a @")
-        }
-    }
+         email = prompt("enter an email")
+         if (email.length > 7 && email.includes("@")) {
+             alert("the log in was seccessfull")
+             const doesEmailExist = userDatabase.some(Element => Element.email === email)
+             if(doesEmailExist){
+                 alert("sorry but this email already exist")
+                } else{
+                    break
+                }
+            }
+            
+            else{
+                alert("the email must contain a @ and be like this exemple exemple@gmail.com...")
+            }
+        };
+        
 
-
-
-
+        
+     
 while(true){
-    var age = prompt("enter your age ")
+     age = prompt("enter your age ")
     if (age && /^[1-9][0-9]?$/.test(age)) {
         alert("the age has been added seccessfully")
-        break
+        break;
     }else{
+        
         alert("the age must contain only numbers but not a 0 or three numbers")
     }
 }
+
+
 while(true){
-    let password = prompt("enter password")
+     password = prompt("enter password")
     if(password){
         alert("the password has been added seccessfully");
         break;
@@ -88,11 +96,11 @@ while(true){
     const theInfo = {
         name:name,
         email:email,
-        Age:age,
-        Password:password
+        age:age,
+        password:password
     }
     userDatabase.push(theInfo);
-  
+   
 
     alert("registration was seccessfull");
 }
@@ -102,9 +110,9 @@ function login() {
     const theEmail = prompt("enter your email");
     const thePassword = prompt("enter your password");
 
-    const dsEmailExiste = userDatabase.find(index => index.email === theEmail)
-    const dsPasswordExiste = userDatabase.find(index => index.password === thePassword)
-  if(dsEmailExiste && dsPasswordExiste){
+    const dsEmailExiste = userDatabase.find(index => index.email === theEmail && index.password === thePassword)
+    // const dsPasswordExiste = userDatabase.find(index => index.password === thePassword)
+  if(dsEmailExiste){
     alert("you just loged in seccessfully")
   }else{
     alert("the email does not existe try to sign up")
@@ -113,18 +121,21 @@ function login() {
 
 
 function changePassword(){
-    const TheEmail = prompt("enter email");
+    const theEmail = prompt("enter email");
     const ThePassword = prompt("enter password");
 
-    const EmailChek = userDatabase.find(x => x.email === TheEmail)
-    const PasswordChek = userDatabase.find(y => y.password === ThePassword)
+    const dsEmailExiste = userDatabase.find(x => x.email === theEmail && x.password === ThePassword)
 
-    if (EmailChek && PasswordChek) {
+
+    if(dsEmailExiste) {
         while(true){
                    const TheNewOne = prompt("enter new password")
-               PasswordChek.password = TheNewOne      
-        break;
-    }    
+              if (TheNewOne) {
+                 dsEmailExiste.password = TheNewOne
+                 alert("the password has been changed")      
+                      break;
+                    }
+                  }    
     }else{
         alert("password or email incorrect")
     }
